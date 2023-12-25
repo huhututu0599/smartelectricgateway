8bc5e382f1d33dc2a95bbea0f0710a7842fb7630
huhututu0599 <1534677036@qq.com>
2023-12-25 21:56:09 +0800

    读取电表数据

---
+++ readelecdata.cpp
@@ -0,0 +1,16 @@
+#include "readelecdata.h"
+
+#define RE 4 // Connect RE terminal with 32 of ESP
+#define DE 5 // Connect DE terminal with 33 of ESP
+#include <SoftwareSerial.h>
+SoftwareSerial mod(16, 17); // RX=16->RO , TX =17
+
+void readelecdata::SetInit()
+{
+    // SoftwareSerial mod(16, 17); // RX=16->RO , TX =17
+    Serial.println("setup->fst");
+    mod.begin(2400, SWSERIAL_8E1);
+    pinMode(RE, OUTPUT);
+    pinMode(DE, OUTPUT);
+    Serial.println("485FST电平已准备就绪");
+}
---
+++ readelecdata.h
@@ -0,0 +1,10 @@
+#ifndef READ_ELEC_DATA_H
+#define READ_ELEC_DATA_H
+
+class readelecdata
+{
+public:
+    void SetInit();
+};
+
+#endif
